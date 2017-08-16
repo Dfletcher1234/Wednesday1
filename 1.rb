a57f04f409f96ce4101006dbd1cc42c9cc0eaa58
David FletcherE <dtsfletcher@gmail.com>
2017-08-16 17:37:41 -0400

    not even close

---
+++ 1.rb
@@ -0,0 +1,13 @@
+class Person
+
+  emotions = {happy:[1,2,3], annoyed:[1,2,3], angry:[1,2,3]}
+
+  @@name = "John"
+  @@emotions = {happy:[1,2,3], annoyed:[1,2,3], angry:[1,2,3]}
+  def initialize
+
+  "john".Person.new 
+
+  end
+
+end
