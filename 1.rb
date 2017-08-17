cbfe0b9e8c5a2d1b0c3a3b05ce37e7dd20cfa810
David FletcherE <dtsfletcher@gmail.com>
2017-08-16 20:48:04 -0400

    Done

--- 1.rb
+++ 1.rb
@@ -1,13 +1,35 @@
+
+
 class Person
 
-  emotions = {happy:[1,2,3], annoyed:[1,2,3], angry:[1,2,3]}
-
-  @@name = "John"
-  @@emotions = {happy:[1,2,3], annoyed:[1,2,3], angry:[1,2,3]}
-  def initialize
-
-  "john".Person.new 
-
+  def initialize(name)
+    @name = name
+    @emotions = {happy:rand(1..3), annoyed:rand(1..3), angry:rand(1..3)}
   end
 
+  def showemot
+    @emotions.each do |k,v|
+    puts "I'm feeling #{level(v)} amount of #{k}"
+    end
+  end
+
+  def level(level)
+      if level == 1
+        return "low"
+      elsif level == 2
+        return "medium"
+      else
+        return "high"
+      end
+  end
+
+
+
+
 end
+
+
+gurjant = Person.new("gurjant")
+p gurjant.inspect
+gurjant.level(2)
+gurjant.showemot
